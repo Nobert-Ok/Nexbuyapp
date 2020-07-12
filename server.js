3a9b94ba11ffe36965eddca903e14fa80ce71cc3
Nobert-Ok <o.chukwuebu@alustudent.com>
2020-07-12 15:47:19 +0200

    here

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express
 
 app.use('/', serveStatic(path.join(__dirname,'/dist')))
 
-const port = process.env.Port || 8080
-app.listen((port))
+const port = process.env.PORT || 8080
+app.listen(port)
 
 console.log('Listening on port: '+ port)
